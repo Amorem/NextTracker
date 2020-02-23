e55c70f9bf3e6d93a08b31b36964024755ba3fa8
Amorem <dev@amorem.com>
2020-02-23 17:09:48 +0100

    HabitButton

---
+++ components/Habit.js
@@ -0,0 +1,18 @@
+import HabitButton from "./HabitButton";
+
+const Habit = () => (
+  <article>
+    <h3>Habit Title</h3>
+    <div>
+      <HabitButton />
+      <HabitButton />
+      <HabitButton />
+      <HabitButton />
+      <HabitButton />
+      <HabitButton />
+      <HabitButton />
+    </div>
+  </article>
+);
+
+export default Habit;
---
+++ components/HabitList.js
@@ -0,0 +1,12 @@
+import Habit from "./Habit";
+
+const HabitList = () => {
+  return (
+    <section>
+      <h2>My Habits</h2>
+      <Habit />
+    </section>
+  );
+};
+
+export default HabitList;
