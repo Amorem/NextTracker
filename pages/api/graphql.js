48a86f3854cc7f665d016545f07b595796e73819
Amorem <dev@amorem.com>
2020-02-25 09:42:08 +0100

    MergeResolvers

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,19 +1,31 @@
 import { ApolloServer, gql } from "apollo-server-micro";
+import { mergeResolvers, mergeTypeDefs } from "graphql-toolkit";
 import connectDb from "../../lib/mongoose";
+import { habitsResolvers } from "../../api/habits/resolvers";
+import { habitsMutations, habitsMutation } from "../../api/habits/mutations";
+import Habits from "../../api/habits/habits.graphql";
 
-const typeDefs = gql`
+const fakeTypeDefs = gql`
   type Query {
     sayHello: String
   }
 `;
 
-const resolvers = {
+const fakeResolvers = {
   Query: {
     sayHello: () => {
       return "Hello";
     }
   }
 };
+
+const resolvers = mergeResolvers([
+  fakeResolvers,
+  habitsResolvers,
+  habitsMutation
+]);
+
+const typeDefs = mergeTypeDefs([fakeTypeDefs, Habits]);
 
 const apolloServer = new ApolloServer({
   typeDefs,
